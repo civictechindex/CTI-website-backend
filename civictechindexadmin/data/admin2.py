36bd259c9d2c68677b60a0ad6256a6901c3c5097
shinjonathan <shin@greenstreetdigital.com>
2020-09-21 21:14:47 -0700

    Adding more explicitly duplicate code

--- civictechindexadmin/data/admin2.py
+++ civictechindexadmin/data/admin2.py
@@ -19,5 +19,21 @@ class LinkAdmin(admin.ModelAdmin):
         return mark_safe(f'<a href="{obj.url}" target="_blank">{obj.url}</a>')
 
 
+class OrganizationAdmin2(admin.ModelAdmin):
+    model = Organization
+    list_display = ('import_id', 'name', 'location', 'parent_organization')
+    list_display_links = ('name', )
+    search_fields = ('name', 'location')
+
+
+class LinkAdmin2(admin.ModelAdmin):
+    model = Link
+    list_display = ('id', 'link_type', 'display_url')
+    list_display_links = ('id', )
+    list_filter = ('link_type', )
+
+    def display_url(self, obj):
+        return mark_safe(f'<a href="{obj.url}" target="_blank">{obj.url}</a>')
+
 admin.site.register(Organization, OrganizationAdmin)
 admin.site.register(Link, LinkAdmin)
