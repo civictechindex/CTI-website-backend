f6d702a965e030b7a5b6f2ff9bbbb0896e25e20f
Cynthia Kiser <cynthiakiser@gmail.com>
2021-10-05 21:29:43 -0700

    Default DEBUG to True until I fix serving static files

--- config/settings.py
+++ config/settings.py
@@ -19,7 +19,7 @@ import socket
 
 env = environ.Env(
     # set casting, default value
-    DEBUG=(bool, False)
+    DEBUG=(bool, True)
 )
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
