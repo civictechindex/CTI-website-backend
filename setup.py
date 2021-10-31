2eb2e4e4c3e45b155e16f171a194dd0573f2e3a7
Cynthia Kiser <cynthiakiser@gmail.com>
2021-10-30 21:02:54 -0700

    add bumpversion and config

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 from setuptools import setup, find_packages
 
 setup(name="CTI-website-backend",
-      version="0.9.0",
+      version="0.5.0",
       description="API server for https://civictechindex.org",
       author="Hack for LA",
       packages=find_packages(exclude=["*.tests", "*.tests.*", "tests.*", "tests", "htmlcov"])
