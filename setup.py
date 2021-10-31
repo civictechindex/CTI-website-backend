77845e05dad0c0b5d38d34835dc30f285ce65eb7
Cynthia Kiser <cynthiakiser@gmail.com>
2021-10-30 21:03:06 -0700

    Bump version: 0.5.0 → 0.5.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 from setuptools import setup, find_packages
 
 setup(name="CTI-website-backend",
-      version="0.5.0",
+      version="0.5.1",
       description="API server for https://civictechindex.org",
       author="Hack for LA",
       packages=find_packages(exclude=["*.tests", "*.tests.*", "tests.*", "tests", "htmlcov"])
